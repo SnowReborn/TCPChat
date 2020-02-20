949048b800729439bcb65dc7f48b07be2439ed92
SnowReborn <36327107+SnowReborn@users.noreply.github.com>
2020-02-19 23:12:46 -0800

    Add files via upload

---
+++ TCP Chatbox test 2/TCP Chatbox test 2/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace TCP_Chatbox_test_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(679, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 75);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "server start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(159, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(189, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(679, 135);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 75);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "connect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(446, 76);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(193, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(155, 159);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(193, 26);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(446, 159);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(193, 26);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(194, 221);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(373, 222);
+            this.textBox5.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(657, 512);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(163, 75);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "send";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(177, 504);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(390, 83);
+            this.textBox6.TabIndex = 8;
+            this.textBox6.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox6_KeyPress);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 651);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+    }
+}
+
---
+++ TCP Chatbox test 2/TCP Chatbox test 2/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// my code start here
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+
+//stop import
+
+
+namespace TCP_Chatbox_test_2
+{
+    public partial class Form1 : Form
+    {
+
+        /*declaring variables*/
+        private TcpClient client;
+        public StreamReader Sreader;
+        public StreamWriter Swriter;
+        public string recieve;
+        public string texttosend;
+
+
+            //end declaring
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //starting my implementation
+            IPAddress[] localIP = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach(IPAddress address in localIP)
+            {
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    textBox1.Text = address.ToString();
+                }
+            }
+            //end of my imp
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("alan is gay");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                TcpListener listener = new TcpListener(IPAddress.Any, int.Parse(textBox2.Text));
+                listener.Start();
+                client = listener.AcceptTcpClient();
+                Sreader = new StreamReader(client.GetStream());
+                Swriter = new StreamWriter(client.GetStream());
+                Swriter.AutoFlush = true;
+
+                backgroundWorker1.RunWorkerAsync();
+                backgroundWorker2.WorkerSupportsCancellation = true;
+            } catch
+            {
+                MessageBox.Show("WHAAAT");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            client = new TcpClient();
+            IPEndPoint IpEnd = new IPEndPoint(IPAddress.Parse(textBox3.Text), int.Parse(textBox4.Text));
+
+            try
+            {
+                client.Connect(IpEnd);
+
+                if (client.Connected)
+                {
+                    textBox5.AppendText("Connected to server" + "\n");
+                    Swriter = new StreamWriter(client.GetStream());
+                    Sreader = new StreamReader(client.GetStream());
+                    Swriter.AutoFlush = true;
+                    backgroundWorker1.RunWorkerAsync();
+                    backgroundWorker2.WorkerSupportsCancellation = true;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (client.Connected)
+            {
+                try
+                {
+                    recieve = Sreader.ReadLine();
+                    this.textBox5.Invoke(new MethodInvoker(delegate ()
+                    {
+                        textBox5.AppendText("You:" + recieve + "\n");
+                    }));
+                    recieve = "";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            if (client.Connected)
+            {
+                Swriter.WriteLine(texttosend);
+                this.textBox5.Invoke(new MethodInvoker(delegate ()
+                {
+                    textBox5.AppendText("Me:" + texttosend + "\n");
+                }));
+            }
+            else
+            {
+                MessageBox.Show("Sending failed");
+            }
+            backgroundWorker2.CancelAsync();
+        }
+
+
+       
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox6.Text != "")
+            {
+                texttosend = textBox6.Text;
+                backgroundWorker2.RunWorkerAsync();
+            }
+            textBox6.Text = "";
+        }
+
+        private void textBox6_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (e.KeyChar == (char)13)
+            {
+                if (textBox6.Text != "")
+                {
+                    texttosend = textBox6.Text;
+                    backgroundWorker2.RunWorkerAsync();
+                }
+                textBox6.Text = "";
+                // Enter key pressed
+            }
+        
+        }
+
+      
+    }
+}
